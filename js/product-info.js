35bbdc66dccb88b868e782999aebc6c2d0c04c62
santiagobenitez65 <santiagobenitez652005@gmail.com>
2025-10-04 17:19:47 -0300

    Hecho el desafiate

--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () { 
+let comments;
+document.addEventListener("DOMContentLoaded", function () {
     let productID = localStorage.getItem("selectedProductId");
     let URL = `https://japceibal.github.io/emercado-api/products/${productID}.json`;
     let COMMENTS_URL = `https://japceibal.github.io/emercado-api/products_comments/${productID}.json`;
@@ -28,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
             images.forEach((imgSrc, index) => {
                 const img = document.createElement("img");
                 img.src = imgSrc;
-                img.classList.add("img-thumbnail", );
+                img.classList.add("img-thumbnail",);
                 img.style.width = "120px";
                 img.style.cursor = "pointer";
 
@@ -56,7 +57,19 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error("Error al cargar info del producto:", error));
 
-        function showComments(commentsArray) {
+
+
+    fetch(COMMENTS_URL)
+        .then(response => response.json())
+        .then(data => {
+            comments = data;
+            showComments(comments);
+        })
+        .catch(error => console.error("Error al cargar comentarios: ", error));
+
+});
+
+function showComments(commentsArray) {
     let htmlContent = "";
 
     for (let comment of commentsArray) {
@@ -73,12 +86,38 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.getElementById("lista-comentarios").innerHTML = htmlContent;
 }
+let rating = 0;
+function setRating(number) {
+    //hace que todas las estrellas sean vacías
+    rating = number;
+    for (let i = 1; i < 6; i++) {
+        let estrella = document.getElementById(`rating-${i}`);
+        estrella.classList.remove("bi-star-fill");
+        estrella.classList.add("bi-star");
+        estrella.innerHTML = `<path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>`
+    }
 
-fetch(COMMENTS_URL)
-    .then(response => response.json())
-    .then(data => {
-        showComments(data);
-    })
-    .catch(error => console.error("Error al cargar comentarios:", error));
+    //hace que las estrellas desde la clickeada para atrás sean rellenas
+    for (let i = 1; i <= number; i++) {
+        let estrella = document.getElementById(`rating-${i}`);
+        estrella.classList.remove("bi-star");
+        estrella.classList.add("bi-star-fill");
+        estrella.innerHTML = `<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>`
+    }
+}
 
-});
+document.getElementById("publicar-comentario").addEventListener("click", () => {
+    //el if es para que no se agregue un comentario al que lke falte rating o texto
+    if (rating == 0 || document.getElementById("input-comentario").value == "") return;
+    let newComment = {};
+    const currentDate = new Date();
+    newComment.product = localStorage.getItem("selectedProductId");
+    newComment.score = rating;
+    newComment.description = document.getElementById("input-comentario").value;
+    newComment.user = sessionStorage.getItem("usuario");
+    newComment.dateTime = `${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDay()} ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
+    comments.push(newComment);
+    showComments(comments);
+    setRating(0)
+    document.getElementById("input-comentario").value = "";
+})
