86b41cf74743cfe4ba9f9cdc0b1abc7f7b13ed4d
naipez <fabrigrana0410@gmail.com>
2025-10-16 21:14:57 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,6 +11,16 @@ document.addEventListener("DOMContentLoaded", function () {
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
+   
+    const nameGuardado = sessionStorage.getItem("name");
+    const lastnameGuardado = sessionStorage.getItem("lastname");
+    const mailGuardado = sessionStorage.getItem("mail");
+    const passwordGuardada = sessionStorage.getItem("password");
+
+    if (!mailGuardado || !passwordGuardada || !lastnameGuardado || !nameGuardado) {
+        window.location.href = "login.html";
+    }
+    
     const usuario = sessionStorage.getItem("name");
     console.log(usuario);
     document.getElementById("usuario-nav").innerText = usuario;
