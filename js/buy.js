bb2f823eedc2023642fa32859ee254d4fb331dee
paulacastro13 <paulacastro6502@gmail.com>
2025-11-13 20:41:54 -0300

    Update buy.js
    Funcionalidad de comprar

--- js/buy.js
+++ js/buy.js
@@ -13,7 +13,7 @@ selectPago.addEventListener("change", () => {
                 <div>
                     <label>Número de tarjeta</label>
                     <div class="contenedor-input">
-                        <input type="text" name="numero">
+                        <input type="text" name="numeroTarjeta">
                     </div>
                 </div>
 
@@ -27,7 +27,7 @@ selectPago.addEventListener("change", () => {
                 <div>
                     <label>Válido hasta</label>
                     <div class="contenedor-input">
-                        <input type="month" name="calle">
+                        <input type="month" name="vencimiento" >
                     </div>
                 </div>
 
@@ -64,4 +64,48 @@ selectPago.addEventListener("change", () => {
                 </div>
             `
     }
-})
+});
+
+//Funcionalidad boton pagar// 
+const departamento = document.getElementById("departamento");
+const localidad = document.getElementById("localidad");
+const calle = document.getElementById("calle");
+const numero = document.getElementById("numero");
+const esquina = document.getElementById("esquina"); 
+const formaPago = document.getElementById("forma-de-pago");
+const tipoEnvio = document.getElementById("tipo-de-envio");
+const confirmar = document.getElementById("confirmar");
+const infoCompra = document.getElementById("info-compra");
+
+confirmar.addEventListener("click", () => {
+    
+    const direccionIncompleta = 
+    departamento.value.trim() === "" ||
+    localidad.value.trim() === "" ||
+    calle.value.trim() === "" ||
+    numero.value.trim() === "" ||
+    esquina.value.trim() === "";
+
+    const envioIncompleto = tipoEnvio.value === "0";
+    const pagoIncompleto = formaPago.value === "0"; 
+
+    let datosPagoVacios = false;
+    const inputsPago = datos.querySelectorAll("input"); 
+    if (inputsPago.length === 0) {
+        datosPagoVacios = true;        
+    } else {
+        datosPagoVacios = Array.from(inputsPago).some(inp => inp.value.trim() === "");
+    }
+
+  if (
+    direccionIncompleta ||
+    pagoIncompleto ||
+    envioIncompleto ||
+    datosPagoVacios
+  ) {
+    console.log(infoCompra.innerHTML);
+    alert("Complete todos los campos para continuar con la compra");
+  } else {
+    alert("¡Compra realizada con éxito! ✅");
+  }
+});
