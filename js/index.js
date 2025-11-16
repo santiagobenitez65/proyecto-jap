346dccf8a6febeefe8e97f608479f84b3650a5e8
paulacastro13 <paulacastro6502@gmail.com>
2025-11-15 22:41:22 -0300

    Update index.js
    Documentación del código

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
+//Cada uno de estos muestra una categoría, almacenada con un ID específico//
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("autos").addEventListener("click", function () {
-        localStorage.setItem("catID", 101);
+        localStorage.setItem("catID", 101); 
         window.location = "products.html"
     });
     document.getElementById("juguetes").addEventListener("click", function () {
@@ -12,11 +13,12 @@ document.addEventListener("DOMContentLoaded", function () {
         window.location = "products.html"
     });
 
+    //Muestra el nombre de usuario en la barra superior//
     const usuario = localStorage.getItem("name");
     console.log(usuario);
     document.getElementById("usuario-nav").innerText = usuario;
 
-
+//Al tocar en el nombre de usuario nos lleva a la pantalla de perfil del usuario//
     document.getElementById("usuario-nav").addEventListener("click", function () {
         window.location = "my-profile.html"
     });
